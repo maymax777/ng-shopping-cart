95e67dcd65333c001ab447aabd265d98429102c8
maymax777 <dev.bugger777@yahoo.com>
2020-10-18 14:06:08 -0400

    Update cart

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,8 @@
+export const FireBaseConfig = {
+  apiKey: "AIzaSyBh3YBQ2CUlmExlVB986xry0I_pebeZPhU",
+  authDomain: "ng-shopping-cart-102d3.firebaseapp.com",
+  databaseURL: "https://ng-shopping-cart-102d3.firebaseio.com",
+  projectId: "ng-shopping-cart-102d3",
+  storageBucket: "ng-shopping-cart-102d3.appspot.com",
+  messagingSenderId: "635122949501",
+};
